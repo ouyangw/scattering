d16639f4f33a93b9e0162abea9f906372eca9c35
Wenjun Ouyang <ouyangw@sas.upenn.edu>
2016-09-28 18:25:32 -0400

    Remove condition to set warning flag as cache in BaseSetup.

--- cmake/BasicSetup.cmake
+++ cmake/BasicSetup.cmake
@@ -24,12 +24,10 @@ elseif (NOT ${PROJNAME}_CXXSTANDARD)
   set (${PROJNAME}_CXXSTANDARD 98 CACHE STRING "c++ standard" FORCE)
 endif (${PROJNAME}_CHECK_CXXSTANDARD)
 
-if (NOT ${PROJNAME}_WARNING_FLAG)
-  set (${PROJNAME}_WARNING_FLAG -pedantic -Wall -Wextra -Wcast-align
-    -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2
-    -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs
-    -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls
-    -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel
-    -Wstrict-overflow=5 -Wswitch-default -Wundef -Werror -Wno-unused
-    CACHE STRING "some warning flag")
-endif (NOT ${PROJNAME}_WARNING_FLAG)
+set (${PROJNAME}_WARNING_FLAG -pedantic -Wall -Wextra -Wcast-align
+  -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2
+  -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs
+  -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls
+  -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel
+  -Wstrict-overflow=5 -Wswitch-default -Wundef -Werror -Wno-unused
+  CACHE STRING "some warning flag")
